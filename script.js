8f25696fcde2c678c38bfa2ac5167ec3a207fb87
beabzk <92110044+beabzk@users.noreply.github.com>
2024-03-02 14:21:19 +0300

    Refactor game logic and add scoring

--- script.js
+++ script.js
@@ -5,26 +5,70 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return "A tie!"
-    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
-        return "You win!"
+        return "tie"
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")) {
+        return "win"
     } else if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
         return "Wrong input";
     } else {
-        return "You lose!"
+        return "lose"
     }
 
 }
 
-const playerSelection = prompt("Enter your choice", "rock");
-const computerSelection = getComputerChoice();
+function getSelection() {
+    // const playerSelection = prompt("Enter your choice", "rock");
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    return [playerSelection, computerSelection]
+}
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 
-// function playGame() {
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+let gameRound = 0;
+let gameResult = "";
 
-// }
+function playGame() {
+
+    let roundResult;
+    let selections;
+
+    // run 5 rounds
+    for (i = 0; i < 5; i++) {
+        selections = getSelection();
+        playerSelection = selections[0].toLowerCase();
+        computerSelection = selections[1].toLowerCase();
+        
+        roundResult = playRound(playerSelection, computerSelection);
+        if (roundResult === "win") {
+            playerScore++;
+        } else if (roundResult === "lose") {
+            computerScore++;
+        } else if (roundResult === "tie") {
+            ties++;
+        }
+        gameRound++;
+    }
+
+    if (playerScore === computerScore) {
+        gameResult = "draw";
+    } else if (playerScore < computerScore) {
+        gameResult = "lose";
+    } else {
+        gameResult = "win";
+    }
+}
+
+playGame();
+
+console.log(`Player: ${playerScore}
+Computer: ${computerScore}
+Draw: ${ties}
+In ${gameRound} rounds`);
